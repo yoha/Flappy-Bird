5757ccb9a3f960aa72bc547d0ae3217debcfd3fb
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-08-11 14:34:28 +0700

    Initial commit & push. Add bird textures and make them animate on the screen.

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -9,34 +9,30 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    // MARK: - Local Properties
+    
+    var bird: SKSpriteNode!
+    
+    // MARK: - Methods Override
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        let birdTexture1 = SKTexture(imageNamed: "bird1")
+        let birdTexture2 = SKTexture(imageNamed: "bird2")
         
-        self.addChild(myLabel)
+        let birdTexturesToBeAnimated = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.3)
+        let animateBirdTextures = SKAction.repeatActionForever(birdTexturesToBeAnimated)
+        
+        bird = SKSpriteNode(texture: birdTexture1)
+        bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        bird.runAction(animateBirdTextures)
+        
+        self.addChild(bird)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
