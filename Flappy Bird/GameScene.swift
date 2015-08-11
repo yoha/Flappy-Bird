df85c546bd149577690f6e0b133826e681332f40
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-08-11 17:03:58 +0700

    Added background that scrolls endlessly.

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -13,21 +13,33 @@ class GameScene: SKScene {
     // MARK: - Local Properties
     
     var bird: SKSpriteNode!
+    var background: SKSpriteNode!
     
     // MARK: - Methods Override
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let birdTexture1 = SKTexture(imageNamed: "bird1")
-        let birdTexture2 = SKTexture(imageNamed: "bird2")
-        
-        let birdTexturesToBeAnimated = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.3)
-        let animateBirdTextures = SKAction.repeatActionForever(birdTexturesToBeAnimated)
+        let birdTexture1 = SKTexture(imageNamed: "flappy1")
+        let birdTexture2 = SKTexture(imageNamed: "flappy2")
+        let animateBirdTextures = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.1)
+        let repeatAnimateBirdTexturesForever = SKAction.repeatActionForever(animateBirdTextures)
         
         bird = SKSpriteNode(texture: birdTexture1)
         bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
-        bird.runAction(animateBirdTextures)
+        bird.runAction(repeatAnimateBirdTexturesForever)
         
+        let backgroundTexture = SKTexture(imageNamed: "background")
+        let animateBackground = SKAction.moveByX(-backgroundTexture.size().width, y: 0, duration: 9)
+        let animateBackgroundReplacement = SKAction.moveByX(backgroundTexture.size().width, y: 0, duration: 0)
+        let repeatAnimateBackgroundForever = SKAction.repeatActionForever(SKAction.sequence([animateBackground, animateBackgroundReplacement]))
+        
+        for var i: CGFloat = 0; i < 3; ++i {
+            background = SKSpriteNode(texture: backgroundTexture)
+            background.position = CGPoint(x: backgroundTexture.size().width / 2 + backgroundTexture.size().width * i, y: CGRectGetMidY(self.frame))
+            background.size.height = self.frame.height
+            background.runAction(repeatAnimateBackgroundForever)
+            self.addChild(background)
+        }
         self.addChild(bird)
     }
     
