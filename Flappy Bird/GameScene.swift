7721e6d276e7f19749ac51f77f682c5089504dce
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-08-12 12:10:51 +0700

    Added: 1) touch gesture to make the bird fly up. 2) pipes obstacles, whose height are randomized. 3) gaps between pipes for birdie to fly through. 4) birdie's zposition to be the foremost sprite in the view stack.

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -12,8 +12,10 @@ class GameScene: SKScene {
     
     // MARK: - Local Properties
     
-    var bird: SKSpriteNode!
     var background: SKSpriteNode!
+    var bird: SKSpriteNode!
+    var ground: SKNode!
+    var pipe: SKSpriteNode!
     
     // MARK: - Methods Override
     
@@ -29,10 +31,10 @@ class GameScene: SKScene {
         let repeatAnimateBackgroundForever = SKAction.repeatActionForever(SKAction.sequence([animateBackground, animateBackgroundReplacement]))
         
         for var i: CGFloat = 0; i < 3; ++i {
-            background = SKSpriteNode(texture: backgroundTexture)
-            background.position = CGPoint(x: backgroundTexture.size().width / 2 + backgroundTexture.size().width * i, y: CGRectGetMidY(self.frame))
-            background.size.height = self.frame.height
-            background.runAction(repeatAnimateBackgroundForever)
+            self.background = SKSpriteNode(texture: backgroundTexture)
+            self.background.position = CGPoint(x: backgroundTexture.size().width / 2 + backgroundTexture.size().width * i, y: CGRectGetMidY(self.frame))
+            self.background.size.height = self.frame.height
+            self.background.runAction(repeatAnimateBackgroundForever)
             self.addChild(background)
         }
         
@@ -45,29 +47,54 @@ class GameScene: SKScene {
         let animateBirdTextures = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.1)
         let repeatAnimateBirdTexturesForever = SKAction.repeatActionForever(animateBirdTextures)
         
-        bird = SKSpriteNode(texture: birdTexture1)
-        bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
-        bird.runAction(repeatAnimateBirdTexturesForever)
+        self.bird = SKSpriteNode(texture: birdTexture1)
+        self.bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        self.bird.runAction(repeatAnimateBirdTexturesForever)
         
         // applying physics (i.e., physics, inertia) to birdie
-        bird.physicsBody = SKPhysicsBody(circleOfRadius: self.bird.size.height / 2)
-        bird.physicsBody!.dynamic = true
-        bird.physicsBody!.allowsRotation = false // <-- disallow the bird to spin
+        self.bird.physicsBody = SKPhysicsBody(circleOfRadius: self.bird.size.height / 2)
+        self.bird.physicsBody!.dynamic = true
+        self.bird.physicsBody!.allowsRotation = false // <-- disallow the bird to spin
+        self.bird.zPosition = 1 // <-- making it the foremost sprite in the view
         self.addChild(bird)
         
         // **********************
         // ******* Ground *******
         // **********************
         
-        let ground = SKNode()
-        ground.position = CGPoint(x: 0, y: 0)
-        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.frame.size.width, height: 1))
-        ground.physicsBody!.dynamic = false // <-- making it immune against gravity
+        self.ground = SKNode()
+        self.ground.position = CGPoint(x: 0, y: 0)
+        self.ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.frame.size.width, height: 1))
+        self.ground.physicsBody!.dynamic = false // <-- making it immune against gravity
         self.addChild(ground)
+        
+        // **********************
+        // ******** Pipes *******
+        // **********************
+        
+        let gapSizeBetweenPipes = self.bird.size.height * 4
+        // each pipe can move (max) up / down 1/4 of the screen's height, which leaves 1/2 of screen's height remaining for movement (1/4 + 1/4 + 1/2 = 1)
+        let rangeOfPipesMovement = arc4random() % UInt32(self.frame.size.height / 2) // <-- between 0 and 1/2 of the screen's height
+        let pipesMovementOffset = CGFloat(rangeOfPipesMovement) - self.frame.size.height / 4 // shifting it down by another 1/4 of the screen's height if needed. meaning it can have a max value of a 1/4 screen's height up & a min value of a 1/4 screen's height down.
+        
+        
+        let pipeTexture1 = SKTexture(imageNamed: "pipe1")
+        self.pipe = SKSpriteNode(texture: pipeTexture1)
+        self.pipe.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame) + self.pipe.size.height / 2 + gapSizeBetweenPipes / 2 + pipesMovementOffset)
+        self.addChild(pipe)
+        
+        let pipeTexture2 = SKTexture(imageNamed: "pipe2")
+        self.pipe = SKSpriteNode(texture: pipeTexture2)
+        self.pipe.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame) - self.pipe.size.height / 2 - gapSizeBetweenPipes / 2 + pipesMovementOffset)
+        self.addChild(pipe)
+        
+        
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
+        self.bird.physicsBody!.velocity = CGVectorMake(0, 0) // <-- set the bird's velocity to 0 so it doesn't fly off the screen when tapped
+        self.bird.physicsBody!.applyImpulse(CGVectorMake(0, 50)) // <-- apply momentum vertically to make the bird "jump"
     }
    
     override func update(currentTime: CFTimeInterval) {
