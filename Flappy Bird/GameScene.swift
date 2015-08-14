2059159c48d52ae081c86e6be842141d8eb0c210
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-08-14 15:07:20 +0700

    Added score counter, score label, & game over alert. Removed collisionBitMask's properties from codebase, changed bitmask values for related local properties & thus fixed ground from letting birdie passes through.

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -15,18 +15,27 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var background: SKSpriteNode!
     var bird: SKSpriteNode!
     var ground: SKNode!
+    var skyLimit: SKNode!
     var pipe: SKSpriteNode!
-    let birdGroup: UInt32 = 1
-    let physicalObjectGroup: UInt32 = 2
+    
+    let skyGroup: UInt32 = 1
+    let birdGroup: UInt32 = 2
+    let physicalObjectGroup: UInt32 = 3
+    let invisibleVerticalGapBetweenPipesGroup: UInt32 = 0
+    
     var isGameOver = false
     let backgroundAndPipesGroupingNode = SKNode()
+    
+    var gameScore = 0
+    let gameScoreLabel = SKLabelNode()
+    let gameOverLabel = SKLabelNode()
     
     // MARK: - Methods Override
     
     override func didMoveToView(view: SKView) {
         
         self.physicsWorld.contactDelegate = self
-        self.physicsWorld.gravity = CGVectorMake(0, -4.5) // <-- default is (0.0, -9.8)
+        self.physicsWorld.gravity = CGVectorMake(0, -2.0) // <-- default is (0.0, -9.8)
         self.addChild(self.backgroundAndPipesGroupingNode)
         
         // **********************
@@ -38,13 +47,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let animateBackgroundReplacement = SKAction.moveByX(backgroundTexture.size().width, y: 0, duration: 0)
         let repeatAnimateBackgroundForever = SKAction.repeatActionForever(SKAction.sequence([animateBackground, animateBackgroundReplacement]))
         
-        for var i: CGFloat = 0; i < 3; ++i {
+        for index in 0...2 {
             self.background = SKSpriteNode(texture: backgroundTexture)
-            self.background.position = CGPoint(x: backgroundTexture.size().width / 2 + backgroundTexture.size().width * i, y: CGRectGetMidY(self.frame))
+            self.background.position = CGPoint(x: backgroundTexture.size().width / 2 + backgroundTexture.size().width * CGFloat(index), y: CGRectGetMidY(self.frame))
             self.background.size.height = self.frame.height
             self.background.runAction(repeatAnimateBackgroundForever)
             self.backgroundAndPipesGroupingNode.addChild(self.background)
         }
+        
+        // **********************
+        // MARK: Game Score Label
+        // **********************
+        
+        self.gameScoreLabel.fontName = "Avenir"
+        self.gameScoreLabel.fontSize = 65
+        self.gameScoreLabel.text = "0"
+        self.gameScoreLabel.position = CGPointMake(CGRectGetMidX(self.frame), self.frame.size.height - 70)
+        self.addChild(self.gameScoreLabel)
         
         // **********************
         // *********** MARK: Bird
@@ -64,10 +83,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.bird.physicsBody!.dynamic = true
         self.bird.physicsBody!.allowsRotation = false // <-- disallow the bird to spin
         self.bird.physicsBody!.categoryBitMask = self.birdGroup
-        self.bird.physicsBody!.collisionBitMask = self.physicalObjectGroup
+//        self.bird.physicsBody!.collisionBitMask = self.birdGroup
         self.bird.physicsBody!.contactTestBitMask = self.physicalObjectGroup
         self.bird.zPosition = 1 // <-- making it the foremost sprite in the view stack
-        self.addChild(bird)
+        self.addChild(self.bird)
         
         // **********************
         // ********* MARK: Ground
@@ -78,7 +97,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.frame.size.width, height: 1))
         self.ground.physicsBody!.dynamic = false // <-- making it immune against gravity
         self.ground.physicsBody!.categoryBitMask = self.physicalObjectGroup
-        self.addChild(ground)
+        self.addChild(self.ground)
+        
+        // **********************
+        // ************ MARK: Sky
+        // **********************
+        
+        self.skyLimit = SKNode()
+        self.skyLimit.position = CGPoint(x: 0, y: self.frame.size.height)
+        self.skyLimit.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.frame.size.width, height: 1))
+        self.skyLimit.physicsBody!.dynamic = false
+        self.skyLimit.physicsBody!.categoryBitMask = self.skyGroup
+        self.addChild(self.skyLimit)
         
         // **********************
         // ********** MARK: Pipes
@@ -90,15 +120,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if !self.isGameOver {
             self.bird.physicsBody!.velocity = CGVectorMake(0, 0) // <-- set the bird's velocity to 0 so it doesn't fly off the screen when tapped
-            self.bird.physicsBody!.applyImpulse(CGVectorMake(0, 50)) // <-- apply momentum vertically to make the bird "jump"
+            self.bird.physicsBody!.applyImpulse(CGVectorMake(0, 30)) // <-- apply momentum vertically to make the bird "jump"
         }
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
-        print("contact")
-        self.isGameOver = true
-        self.bird.speed = 0
-        self.backgroundAndPipesGroupingNode.speed = 0
+        if contact.bodyA.categoryBitMask  == self.invisibleVerticalGapBetweenPipesGroup || contact.bodyB.categoryBitMask == self.invisibleVerticalGapBetweenPipesGroup {
+            ++gameScore
+            self.gameScoreLabel.text = "\(self.gameScore)"
+        }
+        else if contact.bodyA.categoryBitMask == self.skyGroup || contact.bodyB.categoryBitMask == self.skyGroup {
+            return
+        }
+        else {
+            self.isGameOver = true
+            self.bird.speed = 0
+            self.backgroundAndPipesGroupingNode.speed = 0
+            
+            self.gameOverLabel.fontName = "Avenir"
+            self.gameOverLabel.fontSize = 30
+            self.gameOverLabel.text = "Game Over! Tap to play again."
+            self.gameOverLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
+            self.addChild(self.gameOverLabel)
+        }
     }
     
     override func update(currentTime: CFTimeInterval) {
@@ -108,7 +152,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     // MARK: - Custom Methods
     
     func spawnPipes() {
-        let gapSizeBetweenPipes: CGFloat = self.bird.size.height * 4
+        let verticalGapSizeBetweenPipes: CGFloat = self.bird.size.height * 4
         // each pipe can move (max) up / down 1/4 of the screen's height, which leaves 1/2 of screen's height remaining for gap (1/4 + 1/4 + 1/2 = 1)
         let rangeOfPipesVerticalMovement = arc4random() % UInt32(self.frame.size.height / 2) // <-- between 0 and 1/2 of the screen's height
         let pipesVerticalMovementOffset = CGFloat(rangeOfPipesVerticalMovement) - self.frame.size.height / 4 // shifting it down by another 1/4 of the screen's height if needed. meaning it can have a max value of a 1/4 screen's height up & a min value of a 1/4 screen's height down.
@@ -117,18 +161,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let removePipes = SKAction.removeFromParent()
         let animateAndRemovePipes = SKAction.sequence([animatePipe, removePipes])
         
-        self.setupPipe("pipe1", gapSizeBetweenPipes: gapSizeBetweenPipes, pipesVerticalMovementOffset: pipesVerticalMovementOffset, animateAndRemovePipes: animateAndRemovePipes)
-        self.setupPipe("pipe2", gapSizeBetweenPipes: gapSizeBetweenPipes, pipesVerticalMovementOffset: pipesVerticalMovementOffset, animateAndRemovePipes: animateAndRemovePipes)
+        self.setupEachPipe("pipe1", verticalGapSizeBetweenPipes: verticalGapSizeBetweenPipes, pipesVerticalMovementOffset: pipesVerticalMovementOffset, animateAndRemovePipes: animateAndRemovePipes)
+        self.setupEachPipe("pipe2", verticalGapSizeBetweenPipes: verticalGapSizeBetweenPipes, pipesVerticalMovementOffset: pipesVerticalMovementOffset, animateAndRemovePipes: animateAndRemovePipes)
+        
+        // **********************
+        // Invisible Vertical Gaps
+        // **********************
+        
+        let verticalGapBetweenPipes = SKNode()
+        verticalGapBetweenPipes.position = CGPoint(x: CGRectGetMidX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) + pipesVerticalMovementOffset)
+        verticalGapBetweenPipes.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.pipe.size.width, verticalGapSizeBetweenPipes))
+        verticalGapBetweenPipes.runAction(animateAndRemovePipes)
+        verticalGapBetweenPipes.physicsBody!.dynamic = false
+//        verticalGapBetweenPipes.physicsBody!.collisionBritMask = self.birdAndInvisibleVerticalGapBetweenPipesGroup // <-- any 2 object that applies this bitmask will not collide w/ each other but will pass through each other
+        verticalGapBetweenPipes.physicsBody!.categoryBitMask = self.invisibleVerticalGapBetweenPipesGroup
+        verticalGapBetweenPipes.physicsBody!.contactTestBitMask = self.birdGroup
+        self.backgroundAndPipesGroupingNode.addChild(verticalGapBetweenPipes)
     }
     
-    func setupPipe(textureName: String, gapSizeBetweenPipes: CGFloat, pipesVerticalMovementOffset: CGFloat, animateAndRemovePipes: SKAction) {
+    func setupEachPipe(textureName: String, verticalGapSizeBetweenPipes: CGFloat, pipesVerticalMovementOffset: CGFloat, animateAndRemovePipes: SKAction) {
         let pipeTexture = SKTexture(imageNamed: textureName)
         self.pipe = SKSpriteNode(texture: pipeTexture)
         self.pipe.position = textureName == "pipe1"
             ?
-            CGPoint(x: CGRectGetMidX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) + self.pipe.size.height / 2 + gapSizeBetweenPipes / 2 + pipesVerticalMovementOffset)
+            CGPoint(x: CGRectGetMidX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) + self.pipe.size.height / 2 + verticalGapSizeBetweenPipes / 2 + pipesVerticalMovementOffset)
             :
-            CGPoint(x: CGRectGetMidX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) - self.pipe.size.height / 2 - gapSizeBetweenPipes / 2 + pipesVerticalMovementOffset)
+            CGPoint(x: CGRectGetMidX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) - self.pipe.size.height / 2 - verticalGapSizeBetweenPipes / 2 + pipesVerticalMovementOffset)
         self.pipe.physicsBody = SKPhysicsBody(rectangleOfSize: self.pipe.size)
         self.pipe.physicsBody!.dynamic = false
         self.pipe.physicsBody!.categoryBitMask = self.physicalObjectGroup
